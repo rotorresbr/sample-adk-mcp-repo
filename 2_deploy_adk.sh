e0edb70831fd832ed5ac657bbab48b7ce5efd6b6
Rober Torres <rober.assis.torres@gmail.com>
2026-01-14 16:20:29 -0300

    v2-agent-engine

--- 2_deploy_adk.sh
+++ 2_deploy_adk.sh
@@ -1,2 +1,7 @@
 #usa o adk starter pack para preparar e compilar o agente para o agent engine
 uvx agent-starter-pack enhance --adk -d agent_engine
+
+#selected options
+#1. rotorres_agent
+#1. Simple
+#Y
